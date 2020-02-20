fab0bfcb4ea412b59d73a0874b06f53d9e3abcfa
nullgemm <nullgemm@mailbox.org>
2020-02-20 12:25:45 +0100

    clean shader

--- apps/gltf-viewer/shaders/pbr_directional_light.fs.glsl
+++ apps/gltf-viewer/shaders/pbr_directional_light.fs.glsl
@@ -42,13 +42,7 @@ vec4 SRGBtoLINEAR(vec4 srgbIn)
 
 void main()
 {
-  vec4 mrSample = texture2D(
-	  uMetallicRoughnessTexture,
-	  vTexCoords);
-
-  float roughness = mrSample.g * uRoughnessFactor;
-  float metallic = mrSample.b * uMetallicFactor;
-
+  // constants
   vec3 dielectricSpecular = vec3(0.04, 0.04, 0.04);
   vec3 black = vec3(0, 0, 0);
   vec3 N = normalize(vViewSpaceNormal);
@@ -56,21 +50,31 @@ void main()
   vec3 V = normalize(-vViewSpacePosition);
   vec3 H = normalize(L+V);
 
+  // dot products
+  float NdotL = clamp(dot(N, L), 0, 1);
+  float VdotH = clamp(dot(V, H), 0, 1);
+  float NdotV = clamp(dot(N, V), 0, 1);
+  float NdotH = clamp(dot(N, H), 0, 1);
+
+  // metallic/roughness texture
+  vec4 mrSample = texture2D(uMetallicRoughnessTexture, vTexCoords);
+  float roughness = mrSample.g * uRoughnessFactor;
+  float metallic = mrSample.b * uMetallicFactor;
+
+  // color texture
   vec4 baseColorFromTexture =
 	  SRGBtoLINEAR(texture(uBaseColorTexture, vTexCoords));
   vec4 baseColor =
 	  baseColorFromTexture * uBaseColorFactor;
 
-  float NdotL = clamp(dot(N, L), 0, 1);
-  float VdotH = clamp(dot(V, H), 0, 1);
-  float NdotV = clamp(dot(N, V), 0, 1);
-  float NdotH = clamp(dot(N, H), 0, 1);
+  // alpha squared == roughness to the 4
   float a_sq =
 	roughness
 	* roughness
 	* roughness
 	* roughness;
 
+  // compute Vis
   float Vis_sqrt_a =
     sqrt(NdotV * NdotV * (1 - a_sq) + a_sq);
   float Vis_sqrt_b =
@@ -89,20 +93,25 @@ void main()
   	Vis = 0.5 / Vis_denom;
   }
 
+  // V.H to the 5
   float VdotH_p5 = (1 - VdotH);
   VdotH_p5 *= VdotH_p5 * VdotH_p5 * VdotH_p5 * VdotH_p5;
 
+  // variables
   vec3 F0 = mix(dielectricSpecular, baseColor.rgb, metallic);
   vec3 F = F0 + (1 - F0) * VdotH_p5;
   float D = a_sq * M_1_PI * pow((NdotH * NdotH) * (a_sq - 1) + 1, -2);
 
+  // diffuse
   vec3 diffuse = mix(
 	  baseColor.rgb * (1 - dielectricSpecular.r),
 	  black,
 	  metallic) * M_1_PI;
 
+  // components
   vec3 f_diffuse = (1 - F) * diffuse;
   vec3 f_specular = F * Vis * D;
 
+  // done
   fColor = LINEARtoSRGB((f_diffuse + f_specular) * uLightIntensity * NdotL);
 }
